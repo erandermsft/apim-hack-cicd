d00f6613696f34b784a1056fc67611454895e25a
Erik Andersson <erander@microsoft.com>
2022-11-09 18:36:06 +0100

    Update Startup.cs

--- api/CustomerAPI/Startup.cs
+++ api/CustomerAPI/Startup.cs
@@ -24,6 +24,7 @@ namespace CustomerAPI
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            
         }
 
         public IConfiguration Configuration { get; }
